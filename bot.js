0bee1652defdb71f4e8fe408de8e4e3c1a59b9c5
Ttmodz <93042924+Ttmodz@users.noreply.github.com>
2021-10-29 12:47:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2020 Queen-Cutie.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+Queen-Cutie - Thesandu
 */
 
 const fs = require("fs");
