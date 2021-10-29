c799741f7692fe2c19be7942b781fbdf1a5cfa17
Ttmodz <93042924+Ttmodz@users.noreply.github.com>
2021-10-29 11:20:54 +0530

    Add files via upload

---
+++ CutieQueen/index.js
@@ -0,0 +1,16 @@
+/* Copyright (C) 2020 Queen-Cutie.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Queen-Cutie
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
