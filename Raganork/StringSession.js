2cb9d83d326fc45c719b4959d2ba87aa19a65979
Ttmodz <93042924+Ttmodz@users.noreply.github.com>
2021-11-01 13:06:43 +0530

    Update StringSession.js

--- Raganork/StringSession.js
+++ Raganork/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'RAGANORK;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'QUEENCUTIE;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
