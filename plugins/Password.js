1b974924c37253db0fb5c113941fabfc577af7fb
Thesandu Thewsara <93042924+Ttmodz@users.noreply.github.com>
2021-11-03 13:39:03 +0530

    Add files via upload

---
+++ plugins/Password.js
@@ -0,0 +1,31 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.imgur.com/w89FHm7.jpeg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater-Thesandu Thewsara*
+*To check update .update   To update Bot .update now*
+
+*QueenCutie Deploy password : thesandu*
+
+*PASSWORD: _________(thesandu)*
+
+*githublink : https://github.com/Ttmodz/Queen-Cutie*
+`}) 
+
+}));
